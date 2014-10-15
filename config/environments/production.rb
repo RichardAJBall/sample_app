bb44ec605c867635e32e55fadd49ca208861df5c
RichardAJBall <richardajball@hotmail.com>
2014-10-15 15:59:42 +0100

    Finish static pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+
+config.whiny_nils = true
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
