779b55ba425aea855aaa39aa2fc8fbc9a946a689
RichardAJBall <richardajball@hotmail.com>
2014-10-16 10:10:06 +0100

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # for bootstrap as per tutorial
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
